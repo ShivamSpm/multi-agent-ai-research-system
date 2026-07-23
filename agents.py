6b9d6edfee2097125fa1985bd37be351448f29df
ShivamSpm <spmshivam@gmail.com>
2026-07-23 18:28:29 -0400

    Created first agent

---
+++ agents.py
@@ -0,0 +1,19 @@
+from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from tools import web_search , scrape_url 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#model setup 
+llm = ChatOpenAI(model = "gpt-4o-mini",temperature=0)
+
+
+#1st agent 
+def build_search_agent():
+    return create_agent(
+        model = llm,
+        tools= [web_search]
+    )
